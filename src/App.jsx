023d588f8b4651edd1249f1cafcbf7e26f15a019
phucngdev <98637507+phucngdev@users.noreply.github.com>
2024-02-01 21:11:12 +0700

    learn ckeditor

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+// App.jsx / App.tsx
+
+import React, { Component } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Using CKEditor&nbsp;5 build in React</h2>
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor&nbsp;5!</p>"
+          onReady={(editor) => {
+            console.log("Editor is ready to use!", editor);
+          }}
+          onChange={(event) => {
+            console.log(event);
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
